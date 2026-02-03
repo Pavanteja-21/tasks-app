81af942886b769759da6d82a62b8d9fb7159421d
Pavanteja-21 <pavan.singaraju21@gmail.com>
2026-02-03 14:48:03 +0530

    Completed Login page and Register page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,21 @@
 import { Route, Routes } from 'react-router-dom'
 import Navbar from './common/Navbar'
 import Footer from './common/Footer'
+import Register from './pages/Register'
+import Login from './pages/Login'
 
 function App() {
 
   return (
     <>
      <Navbar />
+
+    <Routes>
+      <Route path='/register' element={<Register />} />
+      <Route path='/login' element={<Login />} />
+    </Routes>
+
+
      <Footer />
 
     </>
